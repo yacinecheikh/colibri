6361324ed9d68b1b5360b043ecbfe4b7a3864a26
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2024-07-08 13:42:50 +0000

    implement basic message cryptography

---
+++ client/communication.py
@@ -0,0 +1,19 @@
+import json
+import db
+import system
+
+
+def create_invite(room, address):
+    message = {
+        "room": str(room),
+        "address": str(address),
+        "key": room.key,
+    }
+    message = json.dumps(message)
+    return system.encrypt(address.key, message)
+
+def decode_invite(data, address):
+    decoded = system.decrypt(address.key, data)
+    message = json.loads(decoded)
+    return message
+
