700c56840aa681c726ca7d5b3fde41d61242c305
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2024-07-26 14:07:44 +0000

    refactor fs and gpg calls; add sign/verify functions

--- client/system.py
+++ client/system.py
@@ -29,54 +29,55 @@ def syscall(cmd, stdin=None):
     return p.returncode, out, err
 
 
-"""
-gpg calls
-"""
-
-# TODO: rename (generates an asymetric key pair)
-def create_key():
-    while True:
-        name = str(uuid())
-        if not os.path.exists(f"data/keys/{name}.public.asc"):
-            break
-    syscall(f"./sgpg/create-key data/keys/{name}")
-    return name
-
-# TODO
-def encrypt(key_name, data):
-    return syscall(f"./sgpg/encrypt data/keys/{key_name}.public.asc", message)[1]
-
-#def encrypt(address: Address, data: str):
-#    encrypted = syscall(f"./sgpg/encrypt data/keys/{address.key}.public.asc", data)
-#    return encrypted
-
-def decrypt(key_name, data):
-    return syscall(f"./sgpg/decrypt data/keys/{key_name}.private.asc", message)[1]
-#def decrypt(message: Message):
-#    key = message.address.key
-#    return syscall(f"./sgpg/decrypt data/keys/{key}.private.asc", message.data)[1]
-
-
-# TODO: these commands are not secure
-# any process running "ps aux" can get the passwords of currently running gpg processes
-# additionally, script injections allow the server to generate malicious keys to inject code in bash
-def sym_encrypt(sym_key, message):
-    return syscall(f"gpg --batch --passphrase {sym_key} -c", message)
-
-def sym_decrypt(sym_key, message):
-    return syscall(f"gpg --batch --passphrase {sym_key} -d", message)
-
 
 """
 fs
 """
 
-# import a public key
-def save_key(public_key):
+# keys
+
+def public_key(key_name):
+    return f"data/keys/{key_name}.public.asc"
+
+def private_key(key_name):
+    return f"data/keys/{key_name}.private.asc"
+
+def new_key():
     while True:
         name = str(uuid())
-        if not os.path.exists(f"data/keys/{name}.public.asc"):
-            with open(f'data/keys/{name}.public.asc', 'w') as f:
-                f.write(public_key)
+        if not os.path.exists(public_key(name)):
             return name
+
+def save_key(public, private=None):
+    name = new_key()
+    with open(public_key(name), "w") as f:
+        f.write(public)
+    if private is not None:
+        with open(private_key(name), "w") as f:
+            f.write(private)
+    return name
+
+
+# messages and signatures
+
+def message(name):
+    return f"data/signatures/{name}"
+
+def signature(name):
+    return f"data/signatures/{name}.asc"
+
+def new_message():
+    while True:
+        name = str(uuid())
+        if not os.path.exists(message(name)):
+            return name
+
+def save_message(message_data, signature_data=None):
+    name = new_message()
+    with open(message(name), "w") as f:
+        f.write(message_data)
+    if signature_data is not None:
+        with open(signature(name), "w") as f:
+            f.write(signature_data)
+    return name
 
